72d3eb745b5a542fc272115dfae7de55a5078681
Timothy <negrt@farmingdale.edu>
2021-07-21 21:04:15 -0400

    update: get keys from secrets manager

---
+++ get-keys.py
@@ -0,0 +1,38 @@
+def getKeys():
+
+    secret_name = "SEE SECRETS MANAGER"
+    region_name = "REGION NAME"
+    
+    session = boto3.session.Session()
+    
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            raise e
+    else:
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+        else:
+            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+            
+    secretDict = json.loads(secret)
+    accesstokenkey = secretDict['TokenKey']
+    accesstokensecret = secretDict['TokenSecret']
+    
+    return [accesstokenkey, accesstokensecret]
